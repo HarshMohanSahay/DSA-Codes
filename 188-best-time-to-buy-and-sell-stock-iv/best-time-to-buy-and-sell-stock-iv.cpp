31ca476ca1d40f38f3be7385cf62bc87c989ad10
Harsh Mohan Sahay <75736456+HarshMohanSahay@users.noreply.github.com>
2026-07-11 19:17:46 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (75.65%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,29 +1,31 @@
 class Solution {
 
 public:
-    int maxProfit(int k,vector<int>& prices) {
+    int maxProfit(int k ,vector<int>& prices) {
         int n = prices.size();
         int buy = 1;
-        
+        int cap = 2;
 
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2,vector<int>(k+1, 0)));
+        vector<vector<int>>ahead(2,vector<int>(k+1, 0));
+        vector<vector<int>>curr(2,vector<int>(k+1, 0));
         for (int ind = n - 1; ind >=0; ind--) {
             for (int buy = 1; buy >= 0; buy--) {
                 for(int cap = k;cap>0;cap--){
                 if (buy == 1) {
-                    int takeit = -prices[ind] + dp[ind + 1][0][cap];
-                    int nottakeit = 0 + dp[ind + 1][1][cap];
-                     dp[ind][buy][cap] = max(takeit, nottakeit);
+                    int takeit = -prices[ind] + ahead[0][cap];
+                    int nottakeit = 0 + ahead[1][cap];
+                     curr[buy][cap] = max(takeit, nottakeit);
                 } else {
-                    int takeitsell = prices[ind] + dp[ind + 1][ 1][cap-1];
-                    int nottakeitsell = 0 + dp[ind + 1][0][cap];
-                     dp[ind][buy][cap] = max(takeitsell, nottakeitsell);
+                    int takeitsell = prices[ind] + ahead[ 1][cap-1];
+                    int nottakeitsell = 0 + ahead[0][cap];
+                    curr[buy][cap] = max(takeitsell, nottakeitsell);
                 }
                 }
             }
+            ahead = curr;
             
         }
-        return dp[0][1][k];
+        return ahead[1][k];
 
     }
     };
