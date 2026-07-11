e79aea1ad59e44e1222b9716c8e94a09e7a1c8b0
Harsh Mohan Sahay <75736456+HarshMohanSahay@users.noreply.github.com>
2026-07-11 17:58:06 +0530

    Time: 50 ms (71.63%) | Memory: 79.3 MB (79.40%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -6,24 +6,26 @@ public:
         int buy = 1;
         int cap = 2;
 
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2,vector<int>(3, 0)));
+        vector<vector<int>>ahead(2,vector<int>(3, 0));
+        vector<vector<int>>curr(2,vector<int>(3, 0));
         for (int ind = n - 1; ind >=0; ind--) {
             for (int buy = 1; buy >= 0; buy--) {
                 for(int cap = 2;cap>0;cap--){
                 if (buy == 1) {
-                    int takeit = -prices[ind] + dp[ind + 1][0][cap];
-                    int nottakeit = 0 + dp[ind + 1][1][cap];
-                     dp[ind][buy][cap] = max(takeit, nottakeit);
+                    int takeit = -prices[ind] + ahead[0][cap];
+                    int nottakeit = 0 + ahead[1][cap];
+                     curr[buy][cap] = max(takeit, nottakeit);
                 } else {
-                    int takeitsell = prices[ind] + dp[ind + 1][ 1][cap-1];
-                    int nottakeitsell = 0 + dp[ind + 1][0][cap];
-                     dp[ind][buy][cap] = max(takeitsell, nottakeitsell);
+                    int takeitsell = prices[ind] + ahead[ 1][cap-1];
+                    int nottakeitsell = 0 + ahead[0][cap];
+                    curr[buy][cap] = max(takeitsell, nottakeitsell);
                 }
                 }
             }
+            ahead = curr;
             
         }
-        return dp[0][1][2];
+        return ahead[1][2];
 
     }
     };
