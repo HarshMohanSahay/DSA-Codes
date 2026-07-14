5a73f6dc9e56c9c82d3773700e39d9124241da57
Harsh Mohan Sahay <75736456+HarshMohanSahay@users.noreply.github.com>
2026-07-14 22:39:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (22.86%) - LeetSync

---
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    void evaluate(stack<char>&oper,stack<char>&opn){
+        char op = oper.top();
+        oper.pop();
+        bool ans;
+        if(op=='&'){
+             ans = true;
+             while(opn.top()!='('){
+                ans &= (opn.top()=='t');
+                opn.pop();
+             }
+             opn.pop();
+        }
+         else if(op=='|'){
+              ans = false;
+             while(opn.top()!='('){
+                ans |= (opn.top()=='t');
+                opn.pop();
+             }
+             opn.pop();
+        }
+         else if(op=='!'){
+             ans = (opn.top()=='t');
+             ans = !ans;
+             opn.pop();
+             opn.pop();
+
+        }
+        if(ans==true)opn.push('t');
+        else opn.push('f');
+
+    }
+    bool parseBoolExpr(string expression) {
+        stack<char> oper,opn;
+        int n = expression.size();
+
+        for(int i = 0;i<n;i++){
+            if(expression[i]=='&'){
+                oper.push(expression[i]);
+            }
+            else if(expression[i]=='|'){
+                 oper.push(expression[i]);
+            }
+            else if(expression[i]=='!'){
+                 oper.push(expression[i]);
+            }
+            
+            else if(expression[i]=='('||expression[i]=='t'||expression[i]=='f'){
+                 opn.push(expression[i]);            
+            }
+            else if(expression[i]==')'){
+                 evaluate(oper,opn);
+            }
+        }
+        if(opn.top()=='t')return true;
+        else return false;
+        
+    }
+};
