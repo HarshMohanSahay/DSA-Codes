baa2961c7e94ad8a647229413b74b98072cf6333
Harsh Mohan Sahay <75736456+HarshMohanSahay@users.noreply.github.com>
2026-08-03 19:04:55 +0530

    Time: 381 ms (77.66%) | Memory: 332.8 MB (56.27%) - LeetSync

---
+++ 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void dfs(int u, unordered_map<int, vector<int>>& adj, vector<int>& result) {
+        while (!adj[u].empty()) {
+            int v = adj[u].back();
+            adj[u].pop_back();
+            dfs(v, adj, result);
+        }
+        result.push_back(u);
+    }
+    vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
+        int n = pairs.size();
+        unordered_map<int, vector<int>> adj;
+        unordered_map<int, int> indegree;
+        unordered_map<int, int> outdegree;
+        for (auto& edge : pairs) {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            outdegree[u]++;
+            indegree[v]++;
+        }
+        int start = pairs[0][0];
+        for (auto& it : outdegree) {
+            int node = it.first;
+            int value = it.second;
+            if (value - indegree[node] == 1) {
+                start = node;
+                break;
+            }
+        }
+        vector<int> result;
+
+        dfs(start, adj, result);
+        reverse(result.begin(), result.end());
+        vector<vector<int>> ans;
+        for (int i = 0; i < result.size() - 1; i++) {
+            ans.push_back({result[i], result[i + 1]});
+        }
+        return ans;
+    }
+};
