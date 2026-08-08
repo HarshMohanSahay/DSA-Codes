22ffdd272ff14fff6106ecb675ba972bbac5901f
Harsh Mohan Sahay <75736456+HarshMohanSahay@users.noreply.github.com>
2026-08-08 15:05:17 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (83.41%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -3,12 +3,11 @@ public:
     void moveZeroes(vector<int>& nums) {
         int r = 0;
         int w = 0;
-
-        for(int r =0;r<nums.size();r++){
+       for(int r =0;r<nums.size();r++ ){
             if(nums[r]!=0){
-                swap(nums[w],nums[r]);
+                swap(nums[r],nums[w]);
                 w++;
             }
-        }
+       }
     }
 };
