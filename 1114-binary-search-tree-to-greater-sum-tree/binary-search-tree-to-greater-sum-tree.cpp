9c30d586474d97be908c07cbff78a09bca6b65e8
Harsh Mohan Sahay <75736456+HarshMohanSahay@users.noreply.github.com>
2026-09-01 19:24:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (16.54%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    void postorder(TreeNode* root,int &sum){
+        if(!root) return ;
+        postorder(root->right,sum);
+        sum += root->val;
+        root->val = sum;
+        postorder(root->left,sum);
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        int sum =0;
+        postorder(root,sum);
+        return root;
+    }
+};
