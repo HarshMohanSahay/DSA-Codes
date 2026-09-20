079c0e3ebc9bac8cd44a7fe5f63533fe069f6453
Harsh Mohan Sahay <75736456+HarshMohanSahay@users.noreply.github.com>
2026-09-20 23:13:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (82.07%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
-    long long minEnd(int n, int x) {
-        long long num =x;
-        for(int i =1;i<n;i++){
-            num = (num+1) | x;
+    long minEnd(int n, int x) {
+        long result = x;
+        long remaining = n - 1;
+        long position = 1;
+        
+        while (remaining) {
+            if (!(x & position)) {
+                result |= (remaining & 1) * position;
+                remaining >>= 1;
+            }
+            position <<= 1;
         }
-        return num;
+        
+        return result;
     }
 };
